fb517d37dd8a4be30efd977099d14c68c72d09c5
Stefanie Liu <73271107+stefanieLiu33@users.noreply.github.com>
2023-07-23 18:54:19 +0800

    Add files via upload

---
+++ homework_classroom1.2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+class Student() :
+    # 建構式
+    def __init__(self,name,student_id,age,gender):
+        self.name = name
+        self.student_id = student_id
+        self.age = age
+        self.gender = gender
+
+    # 方法(method)
+    def set_grade(self,grade):
+        self.grade = grade
+        print('grade is set!')
+                      
+    def get_grade(self): # 改用return
+        return print(self.name+'\'s grade is', self.grade)
+        # return self.grade
+    
+    def display_student_info(self):
+        print('student name:',self.name,
+              '\nstudent_id:',self.student_id,
+              '\nage:',self.age,
+              '\ngender:',self.gender,
+              '\ngrade:',self.get_grade())
+
+
+# In[ ]:
+
+
+
+
